f1f2121e2935f82f664f6e678235b3534bd247c1
unknown <sobujhasan1000@gmail.com>
2024-05-21 13:34:03 +0600

    change some file

--- src/app/(withCommonLayout)/products/[productId]/page.tsx
+++ src/app/(withCommonLayout)/products/[productId]/page.tsx
@@ -1,21 +1,21 @@
-import ProductDetail from "@/components/shared/Ui/productDetail/ProductDetail";
+// import ProductDetail from "@/components/shared/Ui/productDetail/ProductDetail";
 
-interface laptopDetails {
-  params: {
-    productId: string;
-  };
-}
+// interface laptopDetails {
+//   params: {
+//     productId: string;
+//   };
+// }
 
-const Detailspage = async ({ params }: laptopDetails) => {
-  // console.log(params);
-  const res = await fetch(`http://localhost:5000/products/${params.productId}`);
-  const laptopDetails = await res.json();
-  console.log(laptopDetails);
-  return (
-    <div>
-      <ProductDetail laptopDetails={laptopDetails} />
-    </div>
-  );
-};
+// const Detailspage = async ({ params }: laptopDetails) => {
+//   // console.log(params);
+//   // const res = await fetch(`http://localhost:5000/products/${params.productId}`);
+//   // const laptopDetails = await res.json();
+//   // console.log(laptopDetails);
+//   return (
+//     <div>
+//       <ProductDetail laptopDetails={laptopDetails} />
+//     </div>
+//   );
+// };
 
-export default Detailspage;
+// export default Detailspage;
