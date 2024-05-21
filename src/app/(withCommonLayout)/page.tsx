c32bede47511524938c6d78222276f200626d57e
unknown <sobujhasan1000@gmail.com>
2024-05-21 14:25:05 +0600

    server external added

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -3,9 +3,12 @@ import FlashSale from "@/components/FlashSale/flashSale";
 import Mostpopular from "@/components/TendingProduct/TendingProducts";
 import TopCatagoris from "@/components/TopBands/TopBand";
 const Homepage = async () => {
-  const res = await fetch("http://localhost:5000/products", {
-    next: { revalidate: 30 },
-  });
+  const res = await fetch(
+    "https://project-test-server-r5ab9c517-sobujhasan1000s-projects.vercel.app/products",
+    {
+      next: { revalidate: 30 },
+    }
+  );
   const products = await res.json();
   // console.log(products);
   return (
